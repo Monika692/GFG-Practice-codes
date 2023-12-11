ee5a9bce87e79561f28fd4659b5748fa17303e94
MONIKA_KUMARI <103176112+Monika692@users.noreply.github.com>
2023-12-11 18:22:03 +0530

    Create Union of two arrays.cpp

---
+++ GFG CODES/Union of two arrays.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class Solution{
+    public:
+int doUnion(int a[], int n, int b[], int m)  {
+    set<int >s;
+     for(int i=0;i<n;i++)
+     {
+        s.insert(a[i]);
+     }
+     for(int j=0;j<m;j++)
+     {
+         s.insert(b[j]);
+     }
+     return s.size();
+    }
+};
